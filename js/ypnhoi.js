becdfce1fc0904365c17ef79996fb4878036e755
Artur Sapek <artur@nnife.com>
2011-10-02 04:01:11 -0400

    added GEO

--- js/ypnhoi.js
+++ js/ypnhoi.js
@@ -3,6 +3,30 @@
 // Artur Sapek
 //
 
+
+
+window.user = new __user__();
+
+
+function __user__(){
+	this.lat = this.lng = null;
+	
+	return this;
+}
+
+function geolocate(){
+	if(geo_position_js.init()){
+		geo_position_js.getCurrentPosition(returnCoords,function(){window.location = "nolocation.html";},{enableHighAccuracy:true});
+	}
+}
+
+
+function returnCoords(p){
+	user.lat = p.coords.latitude;
+	user.lng = p.coords.longitude; 
+}
+
+
 function sanitize(x){
 	x = x.toLowerCase(); //this took me 30 minutes. #idiot
 	var accents = {'\u00E9':'e', '\u00E8':'e', '\u00E0':'a', '\u00E5':'a', '\u00EE':'i', '\u00E7':'c', '\u00F1':'n'}; //lowercase :)
@@ -13,19 +37,3 @@ function sanitize(x){
 	}
 	return x;
 }
-
-function geolocate(){
-	 if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(show_map);
-		} else {
-			window.location = "nolocation.html";
-	}
-}
-function show_map(position) {
-	var latitude = position.coords.latitude;
-	var longitude = position.coords.longitude;
-	console.log(latitude, longitude);
-}
-
-
-geolocate();
