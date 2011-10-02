e4a37bfcf803984fb93d34da502adc40592db264
Artur Sapek <artur@nnife.com>
2011-10-02 09:44:07 -0400

    button works enter doesnt

--- js/app.js
+++ js/app.js
@@ -13,15 +13,26 @@ function onDeviceReady() {
 }
 
 $(document).ready(function() {
-  $("#query").submit(function(e){
-	$.mobile.changePage("#view");
-	console.log("run go");
-	go("40.760196","-73.982019", $('input#venue').val());
-    return false;
+  $("#query").submit(function(){
+submitForm()
   });
+ $("#submitButton").click(function(){
+submitForm()
+  });
+
+
 
   $('#view').live( 'pageshow',function(event, ui){
     var query_venue = $('#venue').attr("value");
     $('#query-venue').html(query_venue);
   });
 });
+
+
+
+function submitForm(){
+		$.mobile.changePage("#view");
+	console.log("run go");
+	go("40.760196","-73.982019", $('input#venue').val());
+    return false;
+   }
