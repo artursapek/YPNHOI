9627a7b45b0dd26e8e5c971f4e849c0c1af8d1ec
Artur Sapek <artur@nnife.com>
2011-10-01 22:23:36 -0400

    hyperPublic.js 3rd attempt lol

--- js/hyperpublic.js
+++ js/hyperpublic.js
@@ -1,3 +1,33 @@
+//
+// hyperPublic namespace 
+// Artur Sapek
+//
+
+function hyperPublic(){
+	this.API = "hyperPublic API: http://developer.hyperpublic.com/places/";
+	this.callJSON = function(query){
+		var params = {q: query};
+		params["client_id"] = "6ZoMqEsQLKisS4C6ZpX65HegIA1tBpttT5vK39dB";
+		params["client_secret"] = "qIKbAlT0osj4C8U3WC5HZMPN7cwnF4MFoAuoz5Hb";
+		params["address"] = "13%20W%203rd%20St%20New%20York,NY";
+		params["limit"] = 50;
+		jQuery.getJSON("https://api.hyperpublic.com/api/v1/places?callback=?",
+ 		params,
+		function(data){
+			for (entry in data){
+				if (data[entry].display_name == params["q"]){
+					console.log(data[entry].display_name, data[entry].perma_link); 
+				}
+			}
+		})	
+	};
+	return this;
+}
+
+
+
+
+
 function createC(a, b, c, d){ // typeof, styles, other attributes, innerHTML. omit options b, c, or d with null
 	var x = document.createElement(a);
 	for (i in b){
@@ -17,9 +47,9 @@ function get(x){
 
 
 function __init__(){
-	$('#query').keypress(function (e) {
-		console.log(e.keyCode);
-	});
+	//$('#query').keypress(function (e) {
+		//console.log(e.keyCode); this makes the script into a keylogger lolol
+	//});
 	$('#submit').click(function(){
 		var query = $('#query').val();
 		jQuery.getJSON("https://api.hyperpublic.com/api/v1/places?callback=?",
@@ -27,21 +57,29 @@ function __init__(){
 		  	client_id: "6ZoMqEsQLKisS4C6ZpX65HegIA1tBpttT5vK39dB",
 		  	client_secret: "qIKbAlT0osj4C8U3WC5HZMPN7cwnF4MFoAuoz5Hb",
 		    address: "13%20W%203rd%20St%20New%20York,NY",
-		    category: query,
-		    radius: "1",
+		    q: query,
+		    radius: "20",
 		    limit: "100"
 		},
 		function(data) {
-			var items = data;
-		    console.log(items);
-		    for (bar in items){
-		    	var t = createC("div", {className: "name"}, items[bar].display_name);
-		    	$("#main").prepend(t);
+				//exact name
+			if($('#exactname').is(':checked')){
+				checkExact(data, query);
+			} else {
+				//non-exact name
+				
+				checkAll(data);
 			}
 		});
-	})
-		
-
+	});
 }
-
-
+function checkExact(data, query){
+	var results = [];
+	for (entry in data){
+		if (data[entry].display_name == query){
+			mainAppend(data[entry]);
+			results.push(data[entry]);
+		}	
+	}
+	console.log(results)
+}
